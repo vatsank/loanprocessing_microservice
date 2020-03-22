4b595c237f4778e605301fc57b0ccd161a832a55
Srivatsan <'vatsank@gmail.com'>
2020-03-22 22:01:14 +0530

    Updated

--- using-profiles/src/main/java/com/example/demo/UsingProfilesApplication.java
+++ using-profiles/src/main/java/com/example/demo/UsingProfilesApplication.java
@@ -24,7 +24,7 @@ public class UsingProfilesApplication {
 		  ctx.close();
 	}
 
-	@Bean
+	@Bean(initMethod = "init")
 	@Profile("dev")
 	public UseProfile  devProfileBean() {
 		
--- using-profiles/src/main/java/com/example/demo/domain/UseProfile.java
+++ using-profiles/src/main/java/com/example/demo/domain/UseProfile.java
@@ -1,14 +1,20 @@
 package com.example.demo.domain;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Slf4j
 public class UseProfile {
 
 	
@@ -16,4 +22,15 @@ public class UseProfile {
 	private int id;
 	@Value("${spring.application.name}")
 	private String application;
+	
+	@Autowired
+	private Environment env;
+
+	public void init() {
+
+		log.info("Init Method called");
+		log.info(env.getActiveProfiles().toString());
+		System.out.println(Arrays.asList(env.getDefaultProfiles().toString()));
+
+		}
 }
